26ce296dc443210ffd735aaa1c5dfca9bf745772
Alice Wangui <wanguialice1952@gmail.com>
2024-07-26 01:46:29 +0300

    Added exception handling for mysql.connector.Error and ensured no SELECT or SHOW statements

--- MySQLServer.py
+++ MySQLServer.py
@@ -23,7 +23,7 @@ def create_database():
             cursor.execute(create_db_query)
             print("Database 'alx_book_store' created successfully!")
 
-    except Error as e:
+    except mysql.connector.Error as e:
         print(f"Error: {e}")
 
     finally:
